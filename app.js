a6921d6b0792bb4942cf1bc82e4f04b8f7fabe60
Dennie de Groot <mail@denniedegroot.nl>
2016-03-05 15:30:50 +0100

    speech-input: add triggers: play, pause, say current song

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 
 var DAAP = require('./lib/daap.js');
 
-var media = require('./lib/media.js');
 var controls = require('./lib/controls.js');
+var media = require('./lib/media.js');
 var settings = require('./lib/settings.js');
+var speech = require('./lib/speech-input.js');
 
 var config = {};
 var daap = new DAAP();
@@ -14,6 +15,7 @@ function init() {
 
     media.init(daap);
     controls.init(daap);
+    speech.init();
 }
 
 module.exports.init = init;
