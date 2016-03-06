393580d92df7c0e5e986266bd73e51f56669a833
Dennie de Groot <mail@denniedegroot.nl>
2016-03-06 19:05:45 +0100

    triggers: add triggers: song changed, paused, resumed
    In order to support the iTunes "push" method for triggers
    I had to implement a session structure. The subscribe_session
    function will block until iTunes sends an update and afterwards
    then calls the triggers.process function.

--- app.js
+++ app.js
@@ -5,17 +5,23 @@ var DAAP = require('./lib/daap.js');
 var controls = require('./lib/controls.js');
 var media = require('./lib/media.js');
 var settings = require('./lib/settings.js');
+var session = require('./lib/session.js');
 var speech = require('./lib/speech-input.js');
+var triggers = require('./lib/triggers.js');
 
 var config = {};
 var daap = new DAAP();
+var sessioninfo = {};
 
 function init() {
-    settings.init(daap, config);
+    settings.init(daap, config, function() {
+        session.init(daap, sessioninfo);
 
-    media.init(daap);
-    controls.init(daap);
-    speech.init();
+        media.init(daap, sessioninfo);
+        controls.init(daap, sessioninfo);
+        triggers.init(daap, sessioninfo);
+        speech.init();
+    });
 }
 
 module.exports.init = init;
