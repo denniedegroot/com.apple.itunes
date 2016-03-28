4f268f661672595feddbb4e672320511840a8457
Maarten Jansen <coldshiver@gmail.com>
2016-03-28 14:33:02 +0200

    homey-mock: merged master

--- app-mock.js
+++ app-mock.js
@@ -11,7 +11,7 @@ function test() {
 }
 
 // Start of original app.js content >>
-"use strict";
+'use strict';
 
 var DAAP = require('./lib/daap.js');
 
@@ -20,20 +20,26 @@ var media = require('./lib/media.js');
 var settings = require('./lib/settings.js');
 var session = require('./lib/session.js');
 var speech = require('./lib/speech-input.js');
-var triggers = require('./lib/triggers.js');
 
-var config = {};
+var triggers = require('./lib/triggers.js');
+var conditions = require('./lib/conditions.js');
+var actions = require('./lib/actions.js');
+
 var daap = new DAAP();
 var sessioninfo = {};
 
 function init() {
-    settings.init(daap, config, function() {
-        setPairingSettings(daap); // << ==== Changed from original
+    settings.init(daap, function () {
         session.init(daap, sessioninfo);
+        //console.log(daap);
 
         media.init(daap, sessioninfo);
         controls.init(daap, sessioninfo);
+
         triggers.init(daap, sessioninfo);
+        conditions.init(daap, sessioninfo);
+        actions.init(daap, sessioninfo);
+
         speech.init();
     });
 }
@@ -41,6 +47,7 @@ function init() {
 module.exports.init = init;
 // << END of original app.js content
 
+//setPairingSettings(daap);
 init();
 test();
 
