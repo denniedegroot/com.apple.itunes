99c9c7e8e9209eae668057e8d57ae33079a560c2
Maarten Jansen <coldshiver@gmail.com>
2016-05-04 21:37:04 +0200

    mock: using injection using rewire framework mock: api realtime works according to athom api reference added: package json with rewire dev dependency

--- app-mock.js
+++ app-mock.js
@@ -3,110 +3,101 @@
 // Mocks the global Homey object.
 require('./lib/mock-homey.js');
 
-// Set your itunes paircode here, host is optional (default is localhost)
+// Rewire gives access to private variables of app.js and lets us change anything we need to run locally.
+var rewire = require('rewire');
+
+// Set your iTunes paircode here, host is optional (default is localhost)
 // Mock will automaticly start pairing if no paircode is set here
 Homey.manager('settings').set('settings', {
-    paircode: '0x4D642AE26714194F',
+    paircode: 'yourpaircodehere',
     host: ''
 });
 
-function test() {
-    setTimeout(function () {
-        // Your test code here
-        media.action_say_currentrating(function () {
-        }, null);
-    }, 2000);
+function myTestCode() {
+    // Your test code here will excecute when session is established with iTunes
+    media.action_say_currentrating(function () {
+    }, null);
 }
 
-// Start of original app.js content >>
-'use strict';
+// Get any private member from app.js we want to use
+var app = rewire('./app.js');
+var settings = app.__get__('settings');
+var media = app.__get__('media');
+var controls = app.__get__('controls');
+var session = app.__get__('session');
+var speech = app.__get__('speech');
+var triggers = app.__get__('triggers');
+var conditions = app.__get__('conditions');
+var actions = app.__get__('actions');
+var daap = app.__get__('daap');
 
-var DAAP = require('./lib/daap.js');
+var wiredSession = rewire('./lib/session.js');
 
-var controls = require('./lib/controls.js');
-var media = require('./lib/media.js');
-var settings = require('./lib/settings.js');
-var session = require('./lib/session.js');
-var speech = require('./lib/speech-input.js');
+overrideCloseSessionIfNeeded();
+app.init();
+triggerPairingIfNoPaircode();
+runTestCodeOnSessionEstablished();
 
-var triggers = require('./lib/triggers.js');
-var conditions = require('./lib/conditions.js');
-var actions = require('./lib/actions.js');
+// Overrides close_session if no paircode is set in settings.
+function overrideCloseSessionIfNeeded() {
+    var paircode = Homey.manager('settings').get('settings').paircode;
 
-var daap = new DAAP();
-var sessioninfo = {};
-
-function init() {
-    settings.init(daap, function () {
-        session.init(daap, sessioninfo);
-
-        media.init(daap, sessioninfo);
-        controls.init(daap, sessioninfo);
-
-        triggers.init(daap, sessioninfo);
-        conditions.init(daap, sessioninfo);
-        actions.init(daap, sessioninfo);
-
-        speech.init();
-    });
-}
-
-module.exports.init = init;
-// << END of original app.js content
-
-// Required variables for settings_changed()
-var config;
-var pair = require('./lib/pairing.js');
-
-// Slightly modified version of settings.settings_changed(name) content >>
-function settings_changed(name, callback) {
-    if (name == 'settings') {
-        config = Homey.manager('settings').get(name);
-
-        if (config.pairstart == false) {
-            daap.host = config.host;
-            daap.paircode = config.paircode;
-        } else {
-            Homey.log('Starting pairing process');
-            pair.code(function (error, data) {
-                if (error) {
-                    return Homey.log(error);
-                }
-
-                config.host = data.host;
-                config.paircode = data.paircode;
-                config.pairstart = false;
-
-                // pair.discoverserver();
-
-                Homey.manager('settings').set('settings', config);
-                Homey.manager('api').realtime('settings_changed', config);
-                Homey.log('Pairing process done');
-                if (callback) {
-                    return callback(data);
-                }
-            });
-        }
+    if (paircode == '' || paircode == 'yourpaircodehere') {
+        wiredSession.__set__('close_session', close_session);
+        wiredSession.__set__('close_session', close_session);
+        app.__set__('session', wiredSession);
     }
 }
-// << END of settings.settings_changed(name)
 
-pairIfNoPaircode();
+// Override close_session function with a lower reconnect timeout
+// Only needed when no pairing code is set on startup.
+function close_session() {
+    // Inject required session members
+    var open_session = wiredSession.__get__('open_session');
+    var session = wiredSession.__get__('session');
+
+    daap.logout(session.id, function (error, response) {
+        if (error) {
+            Homey.log(error);
+        }
+    });
+
+    // pair.discoverserver();
+
+    setTimeout(function () {
+        open_session();
+    }, 3000);
+
+    session.id = 0;
+}
 
 /* Start pairing if no paircode is set, otherwise run test code */
-function pairIfNoPaircode() {
+function triggerPairingIfNoPaircode() {
     var config = Homey.manager('settings').get('settings');
 
     config.pairstart = config.paircode == '' || config.paircode == 'yourpaircodehere';
 
-    if (config.pairstart == true) {
-        settings_changed('settings', function (pairData) {
-            Homey.log(pairData);
-            Homey.log('Enter the paircode uptop in the app-mock.js file and restart the app to run your test code');
-            process.exit();
-        });
-    } else {
-        init();
-        test();
+    if (config.pairstart) {
+        // Will trigger settings_changed and start pairing
+        var defaultSettings = {
+            pairstart: true
+        };
+
+        Homey.manager('settings').set('settings', defaultSettings);
+        Homey.manager('api').realtime('settings_changed', defaultSettings);
     }
+}
+
+// Waits till a valid session is established with iTunes
+// then executes test code once.
+function runTestCodeOnSessionEstablished() {
+    var timer = setInterval(function () {
+        var session = wiredSession.__get__('session');
+
+        if (session && session.id != 0) {
+            clearTimeout(timer);
+            console.log('paircode = ' + daap.paircode);
+            myTestCode();
+        }
+    }, 2000);
 }
